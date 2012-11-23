89df000b4d2a24c308663ca29d0eb36fa9bd7a85
Chris Umbel <chris@chrisumbel.com>
2012-11-23 15:29:13 -0500

    raw data parsing

--- Euler/Problem59.hs
+++ Euler/Problem59.hs
@@ -9,9 +9,27 @@ module Euler.Problem59 where
 
 import Data.Char
 
+{- add the ASCII values of each character in a string -}
 strSum :: String -> Int
 strSum s = sum $ map ord s
 
+{- convert a string of comma delimited ints to a list of ints
+   i.e. "1,2,33" yeilds [1,2,33] -}
+splitInts :: String -> [Int]
+splitInts "" = []
+splitInts s =
+  let firstToken = takeWhile (/= ',') s
+  in read(firstToken) : splitInts (
+  	drop ((length firstToken) + 1) {- everything after the first comma -}
+  	s)
+
+parseCipherText :: String -> [Int]
+parseCipherText s = [0]
+
+decipher :: String -> String -> String
+decipher key cipherText = ""
+
 crack :: (String, Int)
-crack = (plainText, strSum(plainText))
-  where plainText = ""
+crack = 
+  let plainText = ""
+  in (plainText, strSum(plainText))
--- Test/Test59.hs
+++ Test/Test59.hs
@@ -10,12 +10,30 @@ import Test.Framework.Providers.HUnit (testCase)
 import Test.QuickCheck
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 
-import Euler.Problem59 (crack, strSum)
+{- import qualified Data.ByteString.Lazy.Char8 as L -}
+
+import Euler.Problem59 (crack, strSum, splitInts)
 
 main = $(defaultMainGenerator)
 
+{- 
+readCipherText :: String
+readCipherText = L.readFile "./data/cipher1.txt" 
+-}
+
+{- strSum -}
 case_strSum_1_char = 
   strSum "a" @?= 97
 
 case_strSum_2_char = 
   strSum "ab" @?= 97 + 98
+
+{- splitInts -}
+case_splitInts_single = 
+  (splitInts "1") @?= [1]
+
+case_splitInts_singles = 
+  (splitInts "1, 2") @?= [1, 2]
+
+case_splitInts_multiple = 
+  (splitInts "1, 23") @?= [1, 23]
