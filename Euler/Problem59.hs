94410bdfb176042d79319a9e6709f5ae42739ae7
Chris Umbel <chris@chrisumbel.com>
2012-11-24 08:52:57 -0500

    problem 59 works!

--- Euler/Problem59.hs
+++ Euler/Problem59.hs
@@ -45,29 +45,48 @@ decipher key cipherText pos
   | otherwise = chr ((ord (key !! (pos `mod` 3))) `xor` (cipherText !! pos)) : 
     decipher key cipherText (pos + 1) 
 
+{- tokenize a given text and count the number of occurrences of the 10
+   most commonly used English words. -}
 countEnglishWords :: String -> Int
 countEnglishWords s = 
-  {- 10 most common english words from Oxford English Corpus 
+  {- 25 most common english words from Oxford English Corpus 
 	 http://en.wikipedia.org/wiki/Most_common_words_in_English
   -}
-  let englishWords = ["THE", "BE", "TO", "OF", "AND", "A", "IN", "THAT", "HAVE", "I"]
+  let englishWords = ["THE", "BE", "TO", "OF", "AND", "A", "IN", "THAT", "HAVE", "I", "IT", "FOR", "NOT", "ON", "WITH", "HE", "AS", "YOU", "DO", "AT", "THIS", "BUT", "HIS", "BY", "FROM"]
   in 
     length (filter (`elem` englishWords) (tokenize s))
 
-decipherThes :: String -> [Int] -> Int
-decipherThes key cipherText = 
-  let plainText = decipher key cipherText 0
-  in 0
+decipherAndScore :: String -> [Int] -> Int
+decipherAndScore key cipherText = 
+  countEnglishWords (decipher key cipherText 0)
 
 {- iterate a keyspace and decipher a ciphertext with each key. record a score
    for each key of how confident we are that we found the plaintext. -}
 tryCrack :: String -> [String] -> [Int] -> [Int]
 tryCrack key [] cipherText = []
-tryCrack key keyspace cipherText = (decipherThes key cipherText) :
+tryCrack key keyspace cipherText = (decipherAndScore key cipherText) :
   (tryCrack (head keyspace) (tail keyspace) cipherText)
 
-crack :: (String, Int)
-crack = 
-  let plainText = ""
-      keyspace = [[a, b, c] | a <- ['a'..'z'], b <- ['a'..'z'], c <- ['a'..'z']]
-  in (plainText, strSum(plainText))
+maxIndex' :: [Int] -> Int -> Int -> Int
+maxIndex' vals pos _maxIndex
+  | pos > (length vals) - 1 =
+    _maxIndex
+  | (vals !! pos) > (vals !! _maxIndex) =
+    maxIndex' vals (pos + 1) pos
+  | otherwise =
+    maxIndex' vals (pos + 1) _maxIndex
+
+maxIndex :: [Int] -> Int
+maxIndex vals = maxIndex' vals 1 0
+
+crack :: String -> (String, String, Int)
+crack sCipherText = 
+  let keyspace = [[a, b, c] | a <- ['a'..'z'], b <- ['a'..'z'], c <- ['a'..'z']]
+      cipherText = (splitInts sCipherText)
+      winnerIndex = maxIndex (tryCrack (head keyspace) (tail keyspace) cipherText)
+      winner = keyspace !! winnerIndex
+      plainText = decipher winner cipherText 0
+  in (keyspace !! winnerIndex, plainText, strSum plainText)
+      
+
+
